61477e2a43d5de685a4c522d902173a8d643c3dc
andy.xu <xiaohei@maihaoche.com>
2017-10-23 19:27:49 +0800

    Rpc 发送消息数据结构增加

---
+++ bard-transport/src/main/java/com/bard/transport/BardRpcRequest.java
@@ -0,0 +1,48 @@
+package com.bard.transport;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @Author andyXu xu9529@gmail.com
+ * @Date 2017/10/23
+ *
+ * RPC 发送消息类
+ */
+@Builder
+@Data
+public class BardRpcRequest {
+
+    private static AtomicLong REQUEST_ID = new AtomicLong(0L);
+
+    /**
+     * 接口名
+     */
+    private String ifaceName;
+    /**
+     * 方法名
+     */
+    private String methodName;
+    /**
+     * 方法参数
+     */
+    private Object[] args;
+    /**
+     * 方法参数类型
+     */
+    private Class<?>[] paramTypes;
+    /**
+     * 超时时间
+     */
+    private Long timeOut;
+    /**
+     * 请求 id
+     */
+    private long requestId;
+
+    public BardRpcRequest() {
+        this.requestId = REQUEST_ID.incrementAndGet();
+    }
+}
