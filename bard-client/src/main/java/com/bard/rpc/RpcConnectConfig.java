8f4c0f74e535bc9eb7a2cc3ce0f501958f09cec9
andy.xu <xiaohei@maihaoche.com>
2017-10-23 11:15:13 +0800

    新加一个 RpcConnectConfig 连接参数类

---
+++ bard-client/src/main/java/com/bard/rpc/RpcConnectConfig.java
@@ -0,0 +1,36 @@
+package com.bard.rpc;
+
+import lombok.Data;
+
+/**
+ * @Author andyXu xu9529@gmail.com
+ * @Date 2017/10/23
+ * 负责组装连接信息等参数
+ */
+@Data
+public class RpcConnectConfig {
+
+    private String host;
+
+    private String port;
+    /**
+     * 默认超时60s
+     */
+    private Long connectTimeOut = 60000L;
+
+
+    public RpcConnectConfig(String host, String port, Long connectTimeOut) {
+        this.host = host;
+        this.port = port;
+        this.connectTimeOut = connectTimeOut;
+    }
+
+    public RpcConnectConfig(String host, String port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public RpcConnectConfig() {
+    }
+}
+
